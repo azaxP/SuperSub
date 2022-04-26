cf4dc053bcfe1c637c76b72e3f1669cbcc8001b9
azaxP <zpapadov@hotmail.com>
2022-04-26 13:02:35 +0300

    added some comments

--- supeSub.py
+++ supeSub.py
@@ -9,7 +9,7 @@ FORMAT = 'ION'
 LANG = "english"
 IFMATCH = 1
 
-ENC = 'utf_8_sig'
+ENC = 'utf_8_sig'  # UTF with BOM (starts with an identifier string)
 BOM = True
 CRLF = True
 
@@ -21,7 +21,7 @@ KEYS = [k for k in os.listdir(subdir) if os.path.isdir(os.path.join(subdir, k))]
 
 for k in KEYS:
     SUBS = [s for s in os.listdir(os.path.join(subdir, k))]
-    LSUBS = np.array([leven(s.lower(), LANG) for s in SUBS])
+    LSUBS = np.array([leven(s.lower(), LANG) for s in SUBS])  # Identify correct lang for subs
 
     best = np.min(LSUBS)
     howmany = LSUBS == best
@@ -30,10 +30,10 @@ for k in KEYS:
     base = best.split('.')[-1]
     print(k)
 
-    if (not BOM) and (not CRLF):
+    if (not BOM) and (not CRLF):  # Old version: just copy and rename subtitles
         shutil.copy(os.path.join(subdir, k, best),
                     os.path.join(WDIR, '.'.join([k, base])))
-    elif BOM and CRLF:
+    elif BOM and CRLF:  # Phillips compatible: Inserts BOM string and converts LINUX \n to WIN \r\n
         with open(os.path.join(WDIR, '.'.join([k, base])), 'wb') as wf:  # Open new srt for writing
             with open(os.path.join(subdir, k, best),  # Open selected sub
                       mode='r') as rf:
